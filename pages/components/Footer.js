4c4396abc8931f0857fc3af6dd4c8bdbf9bc098e
Raneemoqaily7 <raneemoqaily7@gmail.com>
2022-06-10 02:17:59 +0300

    inprocess

---
+++ pages/components/Footer.js
@@ -0,0 +1,13 @@
+
+
+function Footer() {
+  return (
+    <footer className="px-5 fixed bottom bottom-0 py-6 bg-[#34d399]   w-screen ">
+      <p > &copy; 2022</p>
+      
+    </footer>
+    
+  )
+}
+
+export default Footer
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+
+import Header from "./components/Header" ;
+import Main from "./components/Main" ;
+import Footer from "./components/Footer";
+import Head from "next/head";
+
+
+function Home () 
+{
+  return (
+    <>
+    <Head>
+      <title> Cookie Stand Admin</title>
+    </Head>
+
+    <Header/>
+    
+    
+    <Main/>
+    <Footer/>
+
+
+
+
+    </>
+  )
+  
+}
+
+
+export default Home 
